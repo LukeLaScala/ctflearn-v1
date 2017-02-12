a896f1fe6e71d00134f612b74574923670394eb4
Luke LaScala <lascala.luke@gmail.com>
2017-02-12 00:18:49 -0500

    Overhaul

--- view/head.php
+++ view/head.php
@@ -16,3 +16,13 @@
 <script src="js/nouislider.js"></script>
 <link rel="stylesheet" href="css/nouislider.css">
 <link rel="stylesheet" href="//cdn.jsdelivr.net/font-hack/2.020/css/hack-extended.min.css">
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-86115073-3', 'auto');
+    ga('send', 'pageview');
+
+</script>
